bc48d3490d6b0ebbaeaafe88eaea0af73d29177d
baebumsoo <baebeomsu77@gmail.com>
2023-03-01 21:03:27 +0900

    [#1] setting: scriptt설정

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;  
+    box-sizing: border-box;
+  }
+  html {
+    width: 100%;
+  }
+  body {
+    width: 100%;
+    margin: 0 auto;
+   
+  }
+  ul, li, ol {
+    list-style: none;
+  }
+  
+  a {
+    text-decoration: none;
+    color: #111;
+  }
+ 
+`;
+
+export default GlobalStyle;
