e9b9bc32af94a0a999b930af0ed47b3a5e73ef3c
baebumsoo <baebeomsu77@gmail.com>
2023-03-18 16:38:04 +0900

    [#7]: 스타일 컴포넌트 ssr 가능하도록 세팅

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,51 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react';
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          // eslint-disable-next-line react/jsx-key
+          <div>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </div>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="description" content="My First Static Website" />
+          <meta name="keywords" content="nextjs,static,website" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Catamaran:wght@700&family=Poppins:wght@600&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link rel="shortcut icon" href="/favicon.ico" />
+          {/* <Favicon /> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
