8c1b60a33b72fc644797b6de17ae3726e8424747
RohHeeJin <ranin00421@gmail.com>
2023-03-18 13:19:02 +0900

    [#15] feat: KakaoMap 추가

--- src/components/Write/Contents/index.tsx
+++ src/components/Write/Contents/index.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import ContentsMap from './ContentsMap';
 import * as S from './style';
 
 const WriteContents = () => {
   return (
-    <div>
-      <S.ContentsTitleContainer>
-        <S.ContentsTitle placeholder="제목" />
-      </S.ContentsTitleContainer>
-      <S.ContentsTimeContainer>
-        <div>
+    <S.WriteContainer>
+      <S.TitleContainer>
+        <S.TitleInput placeholder="제목" />
+      </S.TitleContainer>
+
+      <S.TimeContainer>
+        <S.DateWrapper>
           <div>달력</div>
           <div>운동 날짜</div>
-        </div>
-        <div>
+        </S.DateWrapper>
+        <S.TimeWrapper>
           <div>시계</div>
           <div>운동 시간</div>
-        </div>
-      </S.ContentsTimeContainer>
-    </div>
+        </S.TimeWrapper>
+      </S.TimeContainer>
+
+      <ContentsMap />
+    </S.WriteContainer>
   );
 };
 
