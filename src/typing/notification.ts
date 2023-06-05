4b4061152e9716bd3f892bf48e7407da6ca68a9d
taeyoung <rlaxodud911@gmail.com>
2023-06-06 01:30:45 +0900

    [LAS-10] feat: 알림 타입 정의 #63

---
+++ src/typing/notification.ts
@@ -0,0 +1,9 @@
+export interface NotificationType {
+  notificationId: number;
+  recruitmentId: number;
+  recruitmentTitle: string;
+  senderNickname: string;
+  notificationType: string;
+  isRead: boolean;
+  requestDate: string;
+}
