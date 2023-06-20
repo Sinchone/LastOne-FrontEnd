865f34c0313a49b604d2a99a39ef3af1622c18a1
taeyoung <rlaxodud911@gmail.com>
2023-06-21 01:50:51 +0900

    [LAS-29] fix: 액세스 토큰 및 리프레쉬 토큰 만료 시간 수정 #88

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,11 +1,14 @@
 import Cookies from 'js-cookie';
 
+const accessTokenValidTime = new Date(new Date().getTime() + 3 * 60 * 60 * 1000); // 3 hours
+const refreshTokenValidTime = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000); // 7 days
+
 export const setAccessTokenToCookie = (token: string) => {
-  Cookies.set('AccessToken', token, { expires: 7 * 24 * 60 * 60 });
+  Cookies.set('AccessToken', token, { expires: accessTokenValidTime });
 };
 
 export const setRefreshTokenToCookie = (token: string) => {
-  Cookies.set('RefreshToken', token, { expires: 7 * 24 * 60 * 60 });
+  Cookies.set('RefreshToken', token, { expires: refreshTokenValidTime });
 };
 
 export const getAccessTokenFromCookie = () => {
