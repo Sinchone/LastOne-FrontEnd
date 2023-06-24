32a0c11badd953125b69bc1465807b35031a621a
taeyoung <rlaxodud911@gmail.com>
2023-06-24 23:36:15 +0900

    [LAS-43] feat: 로딩 페이지 퍼블리싱 #85

---
+++ src/components/Common/Loading/style.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  svg {
+    width: 100px;
+    height: 100px;
+  }
+`;
