c2ff922dc8e83215b01b7eaafbc0a89aa68b2627
taeyoung <rlaxodud911@gmail.com>
2023-06-26 15:19:58 +0900

    [LAS-36] feat: post와 postdetail 타입 정의 #91

--- src/typing/post.ts
+++ src/typing/post.ts
@@ -1,20 +1,44 @@
 import { times } from '@constants/post';
-import { GymInfoType } from './user';
+import { GymInfoType, FitnessType } from './user';
 
 export interface Post {
+  id: number;
   title: string;
   description: string;
-  workoutPart: string;
+  workoutPurpose: string;
   preferGender: string;
   gym: GymInfoType;
+  workoutPart: string;
   startedAt: StartedAtDate;
+  status: string;
+  imgUrl: string;
 }
 
+export interface PostDetailType {
+  memberId: number;
+  nickname: string;
+  profileUrl: string;
+  workoutPurpose: string;
+  sbd: FitnessType;
+  gym: GymInfoType;
+
+  recruitmentId: number;
+  title: string;
+  description: string;
+  preferGender: string;
+  imgUrls: string[];
+  startedAt: StartedAtDate;
+  createdAt: CreatedAtDate;
+}
 export interface TimeType {
   meridiem: '' | '오전' | '오후';
   time: '' | (typeof times)[number];
 }
 
 export interface StartedAtDate extends TimeType {
+  date: string;
+}
+
+export interface CreatedAtDate extends TimeType {
   date: string;
 }
