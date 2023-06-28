6ac656ccdb36952437c961b6cabd818453bee318
Lami <Halam.Lee.Lami@gmail.com>
2023-06-29 04:38:51 +0900

    [LAS-37] feat: useQuery 이용해 메인페이지 데이터 받아와 페이지 작업 완성 #93

--- src/components/Main/Contents/index.tsx
+++ src/components/Main/Contents/index.tsx
@@ -1,51 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RightArrowIcon from '@assets/icon/right-arrow.svg';
 import { Card } from '@components/Common';
 import * as S from './style';
 import { useGetTodayPartner } from '@hooks/MainPage/queries';
+import useGetPosts from '@hooks/MainPage/queries/useGetPosts';
+import { MainPost } from '@typing/post';
+import { getAccessTokenFromCookie } from '@utils/token';
 
 const Contents = () => {
-  const { data } = useGetTodayPartner();
-  console.log('test', data);
+  const [todayPartner, setTodayPartner] = useState({
+    recruitmentId: null,
+    partnerName: '',
+    workoutPart: '',
+    startedAt: '',
+    gym: '',
+  });
+
+  const accessToken = getAccessTokenFromCookie();
+  const { data: todayPartnerData } = useGetTodayPartner();
+
+  useEffect(() => {
+    if (accessToken) {
+      console.log('test', todayPartnerData?.data);
+      setTodayPartner({ ...todayPartnerData?.data.data });
+    }
+  }, [accessToken, todayPartnerData]);
+
+  const { data: mainPosts } = useGetPosts();
+  const mainPostArray = mainPosts?.data.data;
 
   return (
     <S.Wrapper>
+      {accessToken && mainPostArray && (
+        <S.Content>
+          <S.Title href={'/apply'}>
+            <span>오늘의 약속</span>
+            <RightArrowIcon />
+          </S.Title>
+          <S.PromiseContents>
+            <S.PromiseContent>
+              <span>파트너</span>
+              <span className="description">{todayPartner.partnerName} 님</span>
+            </S.PromiseContent>
+            <S.Contour />
+            <S.PromiseContent>
+              <span>운동부위</span>
+              <span className="description">{todayPartner.workoutPart}</span>
+            </S.PromiseContent>
+            <S.Contour />
+            <S.PromiseContent>
+              <span>운동날짜</span>
+              <span className="description">{todayPartner.startedAt}</span>
+            </S.PromiseContent>
+            <S.Contour />
+            <S.PromiseContent>
+              <span>위치</span>
+              <span className="description">{todayPartner.gym}</span>
+            </S.PromiseContent>
+          </S.PromiseContents>
+        </S.Content>
+      )}
       <S.Content>
-        <S.Title>
-          <span>오늘의 약속</span>
-          <RightArrowIcon />
-        </S.Title>
-        <S.PromiseContents>
-          <S.PromiseContent>
-            <span>파트너</span>
-            <span className="description">차무식 님</span>
-          </S.PromiseContent>
-          <S.Contour />
-          <S.PromiseContent>
-            <span>운동부위</span>
-            <span className="description">어깨</span>
-          </S.PromiseContent>
-          <S.Contour />
-          <S.PromiseContent>
-            <span>운동날짜</span>
-            <span className="description">2023.03.03.20:00</span>
-          </S.PromiseContent>
-          <S.Contour />
-          <S.PromiseContent>
-            <span>위치</span>
-            <span className="description">스포애니 성산점</span>
-          </S.PromiseContent>
-        </S.PromiseContents>
-      </S.Content>
-      <S.Content>
-        <S.Title>
+        <S.Title href={'/matching'}>
           <span>파트너 찾기</span>
           <RightArrowIcon />
         </S.Title>
         <S.CardList>
-          {Array.from({ length: 9 }, (_, index) => index).map((el) => (
-            <Card key={el} size={'main'} />
-          ))}
+          {mainPostArray && mainPostArray.length > 0
+            ? mainPostArray.map((post: MainPost) => <Card key={post.id} size={'main'} {...post} />)
+            : '모집중인 파트너가 없습니다.'}
         </S.CardList>
       </S.Content>
     </S.Wrapper>
--- src/constants/querykeys.ts
+++ src/constants/querykeys.ts
@@ -2,4 +2,5 @@ export const queryKeys = {
   signUp: ['signUp'],
   me: ['me'],
   partner: ['partner'],
+  mainPost: ['mainPost'],
 } as const;
