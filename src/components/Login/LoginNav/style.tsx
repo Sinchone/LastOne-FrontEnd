416ae5cce6716bfd88ac94431a2caf7fe87ca11c
taeyoung <rlaxodud911@gmail.com>
2023-07-07 22:53:49 +0900

    [LAS-14] refactor: 공통된 디자인 통합 #107

--- src/components/Login/LoginNav/style.tsx
+++ src/components/Login/LoginNav/style.tsx
@@ -31,21 +31,15 @@ export const ButtonContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 24px;
-
-  button {
-    cursor: pointer;
-  }
 `;
 
-export const KakaoBtn = styled.button`
+const Button = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
 
   width: 100%;
   height: 62px;
-
-  background-color: var(--color-yellow);
 
   border: 0px;
   border-radius: 6px;
@@ -55,31 +49,18 @@ export const KakaoBtn = styled.button`
   line-height: 30px;
   letter-spacing: -2%;
 
+  cursor: pointer;
+
   svg {
     margin-right: 4px;
   }
 `;
 
-export const HomeBtn = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+export const KakaoBtn = styled(Button)`
+  background-color: var(--color-yellow);
+`;
 
-  width: 100%;
-  height: 62px;
-
+export const HomeBtn = styled(Button)`
   background-color: var(--color-white);
   color: var(--color-primary-main);
-
-  border: 0px;
-  border-radius: 6px;
-
-  font-weight: 400;
-  font-size: 20px;
-  line-height: 30px;
-  letter-spacing: -2%;
-
-  svg {
-    margin-right: 4px;
-  }
 `;
