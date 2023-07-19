dd4c4cba932e5da0bd6fd259f8a557aca972a675
taeyoung <rlaxodud911@gmail.com>
2023-07-19 17:13:59 +0900

    [LAS-77] feat: 모집중만 보기 체크박스 컴포넌트 분리 #120

---
+++ src/components/Matching/MatchingFilter/IsRecruiting/style.tsx
@@ -0,0 +1,15 @@
+import theme from '@styles/theme';
+import styled from 'styled-components';
+
+export const CheckBoxWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const UnChecked = styled.div`
+  width: 13px;
+  height: 13px;
+  border: solid var(--color-gray8);
+  display: flex;
+`;
