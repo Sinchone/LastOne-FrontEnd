049e0a5f13ff7202ed3a69706705fac99a2913a1
Lami <Halam.Lee.Lami@gmail.com>
2023-07-22 17:05:15 +0900

    [LAS-25] feat: BottomSheet - Calendar 컴포넌트 작성 #124

--- src/components/Common/BottomSheet/BottomSheetList/Calendar/index.tsx
+++ src/components/Common/BottomSheet/BottomSheetList/Calendar/index.tsx
@@ -5,14 +5,26 @@ import BottomArrowIcon from '@assets/icon/bottom-arrow.svg';
 import { useBottomSheet } from '@hooks/common';
 import ReactCalendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-
-type ValuePiece = Date | null;
+import moment from 'moment';
+import { useRouter } from 'next/router';
+import { MATCHING_PAGE } from '@constants/route';
 
 const Calendar = () => {
   const { closeBottomSheet } = useBottomSheet();
-  const [value, onChange] = useState<ValuePiece | [ValuePiece, ValuePiece]>(new Date());
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+  const router = useRouter();
 
-  console.log(value);
+  const handleDateChange = (date: Date) => {
+    setSelectedDate(date);
+  };
+
+  const handleApply = () => {
+    router.push({
+      pathname: MATCHING_PAGE,
+      query: { ...router.query, date: moment(selectedDate).format('YYYY.MM.DD') },
+    });
+    closeBottomSheet();
+  };
 
   return (
     <S.BottomSheetContent>
@@ -25,12 +37,18 @@ const Calendar = () => {
       </S.BottomSheetHeader>
       <S.Content>
         <S.CalendarWrapper>
-          <ReactCalendar onChange={onChange} value={value} />
+          <ReactCalendar
+            onChange={handleDateChange as any}
+            value={selectedDate ? selectedDate : new Date()}
+            calendarType={'US'}
+            formatMonthYear={(locale, date) => moment(date).format('YYYY.MM')}
+            formatDay={(locale, date) => moment(date).format('D')}
+          />
         </S.CalendarWrapper>
       </S.Content>
       <S.ButtonGroup>
-        <S.CancelButton>취소</S.CancelButton>
-        <S.Button>적용</S.Button>
+        <S.CancelButton onClick={closeBottomSheet}>취소</S.CancelButton>
+        <S.Button onClick={handleApply}>적용</S.Button>
       </S.ButtonGroup>
     </S.BottomSheetContent>
   );
