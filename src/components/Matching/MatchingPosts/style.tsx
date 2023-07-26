e6aedaf958cd55a017b0f9422bd3188abe7e42e5
taeyoung <rlaxodud911@gmail.com>
2023-07-26 17:31:18 +0900

    [LAS-77] design: 모집글 목록 가운데 정렬 #120

--- src/components/Matching/MatchingPosts/style.tsx
+++ src/components/Matching/MatchingPosts/style.tsx
@@ -3,17 +3,20 @@ import styled from 'styled-components';
 export const Wrapper = styled.section`
   width: 100%;
   min-height: calc(100vh - 300px);
-  padding: 30px 6px;
+  padding: 0 6px;
   padding-bottom: 100px;
+
+  display: flex;
+  justify-content: center;
 `;
 
 export const CardList = styled.div`
-  width: 100%;
+  width: calc(100% - 60px);
   min-height: 100%;
 
   display: flex;
+  justify-content: space-around;
   flex-wrap: wrap;
-  padding-left: 25px;
   gap: 20px;
 `;
 
