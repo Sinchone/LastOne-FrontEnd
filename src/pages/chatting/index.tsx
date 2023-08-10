a30fc840bcdf6c0babd29d171e57961384259c79
JeonghoPark <pjhpjh0805@naver.com>
2023-08-11 05:01:05 +0900

    feat: 채팅 리스트 페이지 작업 #138

--- src/pages/chatting/index.tsx
+++ src/pages/chatting/index.tsx
@@ -1,11 +1,36 @@
 import { MainLayout } from '@components/Common';
 import { ChattingList } from '@components/Chatting';
+import { useGetChattingList } from '@hooks/chatting';
+import { useState, useEffect } from 'react';
+import { Loader } from '@components/Common';
 import React from 'react';
 
 const Chatting = () => {
+
+  const { data: chattingListData, isError, refetch } = useGetChattingList();
+  const [isPossibleToRendering, setIsPossibleToRendering] = useState(false);
+  
+  useEffect(() => {
+    if (isError) {
+      setIsPossibleToRendering(true);
+    }
+    if (chattingListData) {
+      setIsPossibleToRendering(true);
+    } 
+    if (!isPossibleToRendering) {
+      refetch();
+    }
+  }, [isError, chattingListData, isPossibleToRendering, refetch]);
+
+  if (!isPossibleToRendering) {
+    return <Loader />;
+  }
+
+  const chattingList = chattingListData?.data;
+
   return (
     <MainLayout>
-      <ChattingList />
+      <ChattingList chattingList={chattingList} />
     </MainLayout>
   );
 };
