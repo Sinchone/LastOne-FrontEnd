d661e3b3795981d30a9e41c8fa37619ba61e9bf9
JeonghoPark <pjhpjh0805@naver.com>
2023-08-11 20:57:46 +0900

    feat: 채팅방 삭제 관련 API구현 #138

--- src/apis/chatting.ts
+++ src/apis/chatting.ts
@@ -3,3 +3,7 @@ import authApi from '@apis/auth';
 export const getChattingList = () => {
     return authApi.get('/chat/room');
 }
+
+export const deleteChattingRoom = (roomId: string) => {
+    return authApi.delete(`/chat/room/${roomId}`);
+}
