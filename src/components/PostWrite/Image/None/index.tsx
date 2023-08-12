942638008c65c6495858c00b315fe4280ef6808a
taeyoung <rlaxodud911@gmail.com>
2023-08-13 04:17:26 +0900

    [LAS-33] feat: 이미지를 한 번에 올리는 컴포넌트 작성 #98

--- src/components/PostWrite/Image/None/index.tsx
+++ src/components/PostWrite/Image/None/index.tsx
@@ -1,7 +1,25 @@
 import * as S from './style';
 import AddImgIcon from '@assets/icon/addImg.svg';
 
-const NoImage = () => {
+interface Props {
+  setImgFiles: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const NoImage = ({ setImgFiles }: Props) => {
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files as any;
+    const maxLength = files.length > 3 ? 3 : files.length;
+    const images: string[] = [];
+
+    if (files.length > 3) alert('최대 3장까지 첨부할 수 있습니다.');
+
+    for (let i = 0; i < maxLength; i++) {
+      images.push(files[i]);
+    }
+
+    setImgFiles(images);
+  };
+
   return (
     <S.Wrapper>
       <label htmlFor="image-input">
@@ -11,11 +29,10 @@ const NoImage = () => {
           name="image"
           multiple
           accept=".gif, .jpg, .png, .jpeg"
-          // onChange={handleImageChange}
+          onChange={handleImageChange}
         />
+        <AddImgIcon />
       </label>
-      <AddImgIcon />
-      <div></div>
       <div>
         <S.Image></S.Image>
         <S.Image></S.Image>
