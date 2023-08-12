e190447e8d803e19668a187710c4fd526c250f00
JeonghoPark <pjhpjh0805@naver.com>
2023-08-12 17:01:13 +0900

    fix: img 태그에서 next.js가 제공하는 Image 태그로 수정 #138

--- src/components/Chatting/Content/index.tsx
+++ src/components/Chatting/Content/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import * as S from './style';
 import ProfileIcon from '@assets/icon/profile.svg';
-import { useRouter } from 'next/router';
+import Image from 'next/image';
 import MenuIcon from '@assets/icon/list.svg';
+import { useRouter } from 'next/router';
 import { ChattingListType } from '@typing/chatting';
 import { createImageUrl } from '@utils/createImageUrl';
 import { ChattingBottomSheet } from '@components/Chatting/BottomSheet';
 import { calDate } from '@utils/calDate';
 import { createPortal } from 'react-dom';
-import { useEffect, useRef, useState} from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 interface Props {
   chattingList: ChattingListType[];
@@ -37,52 +38,59 @@ const ChattingList = ({ chattingList: initialChattingList }: Props) => {
 
   return (
     <S.Wrapper>
-        {chattingList.length > 0 ? (
-          chattingList.map((c) => (
-            <S.ChatItemWrapper key={c.roomId}>
-              <S.ProfileWrapper>
-                <S.ProfileButton
-                  onClick={() => {
-                    router.push(`/mypage/${c.otherUserId}`);
-                  }}
-                >
-                  <S.ImageWrapper>
-                    {c.profileUrl ? (
-                      <img src={createImageUrl(c.profileUrl as string)} alt="profileImg" />
-                    ) : (
-                      <ProfileIcon />
-                    )}
-                  </S.ImageWrapper>
-                </S.ProfileButton>
-                <S.NameAndMessage>
-                  <span className="name">{c.nickname}</span>
-                  <span className="message">{c.lastChat}</span>
-                </S.NameAndMessage>
-              </S.ProfileWrapper>
-              <S.MenuWrapper>
-                <S.TimeAndNotRead>
-                  <span>{calDate(c.lastChatTime)}</span>
-                  <S.NotRead>{c.notReadCount}</S.NotRead>
-                </S.TimeAndNotRead>
-                <S.MenuIconWrapper
-                  onClick={() => {
-                    setSelectedRoomId(c.roomId);
-                    setBottomSheetVisible(true);
-                  }}
-                >
-                  <MenuIcon />
-                </S.MenuIconWrapper>
-              </S.MenuWrapper>
-            </S.ChatItemWrapper>
-          ))
-        ) : (
-          <S.NoChattingRoom>채팅방이 존재하지 않습니다.</S.NoChattingRoom>
+      {chattingList.length > 0 ? (
+        chattingList.map((c) => (
+          <S.ChatItemWrapper key={c.roomId}>
+            <S.ProfileWrapper>
+              <S.ProfileButton
+                onClick={() => {
+                  router.push(`/mypage/${c.otherUserId}`);
+                }}
+              >
+                <S.ImageWrapper>
+                  {c.profileUrl ? (
+                    <Image
+                      src={createImageUrl(c.profileUrl as string)}
+                      width={500}
+                      height={500}
+                      alt='profileImg'
+                    />
+                  ) : (
+                    <ProfileIcon />
+                  )}
+                </S.ImageWrapper>
+              </S.ProfileButton>
+              <S.NameAndMessage>
+                <span className="name">{c.nickname}</span>
+                <span className="message">{c.lastChat}</span>
+              </S.NameAndMessage>
+            </S.ProfileWrapper>
+            <S.MenuWrapper>
+              <S.TimeAndNotRead>
+                <span>{calDate(c.lastChatTime)}</span>
+                <S.NotRead>{c.notReadCount}</S.NotRead>
+              </S.TimeAndNotRead>
+              <S.MenuIconWrapper
+                onClick={() => {
+                  setSelectedRoomId(c.roomId);
+                  setBottomSheetVisible(true);
+                }}
+              >
+                <MenuIcon />
+              </S.MenuIconWrapper>
+            </S.MenuWrapper>
+          </S.ChatItemWrapper>
+        ))
+      ) : (
+        <S.NoChattingRoom>채팅방이 존재하지 않습니다.</S.NoChattingRoom>
+      )}
+      {isBottomSheetVisible &&
+        selectedRoomId &&
+        mainAreaRef.current &&
+        createPortal(
+          <ChattingBottomSheet onDelete={handleDeleteChatRoom} roomId={selectedRoomId} onClose={closeBottomSheet} />,
+          mainAreaRef.current
         )}
-        {isBottomSheetVisible && selectedRoomId && mainAreaRef.current &&
-          createPortal(
-            <ChattingBottomSheet onDelete={handleDeleteChatRoom} roomId={selectedRoomId} onClose={closeBottomSheet} />,
-            mainAreaRef.current
-          )}
     </S.Wrapper>
   );
 };
