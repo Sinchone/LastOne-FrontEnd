5d7665d08d19618347398b1f5a9907d7c4f904cb
JeonghoPark <pjhpjh0805@naver.com>
2023-08-12 17:02:18 +0900

    style: header와 navibar을 제외한 Content 부분의 min-height 설정 #138

--- src/components/Chatting/Content/style.tsx
+++ src/components/Chatting/Content/style.tsx
@@ -6,7 +6,7 @@ export const Wrapper = styled.div`
   flex-direction: column;
   width: 100%;
   height: 100%;
-  min-height: calc(100vh - 75px);
+  min-height: calc(100vh - 130px);
 `;
 
 export const ChattingMessage = styled.div`
