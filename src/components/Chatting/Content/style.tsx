43c793a387ab2de1e3b841df5560e792128c9e37
JeonghoPark <pjhpjh0805@naver.com>
2023-08-12 11:24:30 +0900

    style: 채팅방 리스트 없을 경우 관련 메세지 나올 때 스크롤바 안생기도록 수정 #138

--- src/components/Chatting/Content/style.tsx
+++ src/components/Chatting/Content/style.tsx
@@ -6,7 +6,7 @@ export const Wrapper = styled.div`
   flex-direction: column;
   width: 100%;
   height: 100%;
-  min-height: calc(100vh - 60px);
+  min-height: calc(100vh - 75px);
 `;
 
 export const ChattingMessage = styled.div`
