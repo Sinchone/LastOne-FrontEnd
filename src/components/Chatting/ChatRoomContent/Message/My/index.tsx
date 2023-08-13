6e3dd5cce06ca386bf9453d47592dfed9f2450fe
JeonghoPark <pjhpjh0805@naver.com>
2023-08-14 00:59:21 +0900

    feat: 채팅방 상세 본문에 사용자 자신의 메세지 관련 컴포넌트 퍼블리싱 #140

---
+++ src/components/Chatting/ChatRoomContent/Message/My/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './style';
+
+
+const MyMessage = () => {
+    return (
+        <S.MessageWrapper>
+            <S.SendTime>오후 10:30</S.SendTime>
+            <S.Message>안녕하세요최대글자테스ㅁㄴ어ㅏ리먼아ㅣ;럼ㄴ;ㅇ러미어림어리;멍라ㅣ먼아ㅣㄹ;ㅏ머ㅏㅇ;럼ㄴ;ㅇ럼ㄴ;아러;ㅁ얼미얼;ㅁ</S.Message>
+        </S.MessageWrapper>
+    )
+}
+
+export default MyMessage
---
+++ src/components/Chatting/ChatRoomContent/Message/My/style.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const MessageWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end; 
+    margin-top: 30px;
+`
+
+export const Message = styled.div`
+    border-radius: 10px 0px 10px 10px;
+    background-color: white;
+    padding: 20px;
+    margin-right: 20px;
+    max-width: 300px;  // 최대 너비 설정
+    box-shadow: -1px 5px 10px rgba(0, 0, 0, 0.5);
+`
+
+
+export const SendTime = styled.div`
+    align-self: flex-end;  // 밑으로 위치하게 함
+    margin-right: 10px;
+    color: #888A8F;
+`
