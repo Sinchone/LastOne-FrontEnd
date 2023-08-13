c9e0b0146ee9a120cd058bcf1cd5b231dfa859ab
JeonghoPark <pjhpjh0805@naver.com>
2023-08-14 01:01:35 +0900

    feat: 채팅방 상세 본문에 날짜 분리선 컴포넌트 퍼블리싱 #140

---
+++ src/components/Chatting/ChatRoomContent/DateLine/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style';
+
+const DateLine = () => {
+    return (
+        <S.DateSepatator>
+            <span>6월 13일</span>
+        </S.DateSepatator>
+    )
+}
+
+export default DateLine
---
+++ src/components/Chatting/ChatRoomContent/DateLine/style.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const DateSepatator = styled.div`
+    position: relative;
+    text-align: center;
+    margin: 30px 0;  // 위아래 여백 설정. 필요에 따라 조절
+
+    &::before, &::after {
+        content: '';
+        position: absolute;
+        top: 50%;
+        width: 35%;  // 선의 길이. 필요에 따라 조절
+        height: 1px;  // 선의 두께
+        background-color: #ccc;  // 선의 색상
+    }
+
+    &::before {
+        left: 10px;
+    }
+
+    &::after {
+        right: 10px;
+    }
+
+    span {
+        background-color: transparent;  // 배경색. 채팅창의 배경색과 동일하게 설정
+        padding: 0 10px;  // 양옆의 여백
+        z-index: 1;
+        position: relative;
+        color: #888A8F;
+    }
+`
