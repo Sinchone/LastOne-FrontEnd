cf851635f5ed4460a21c882468ae9bcb97ad0b9f
JeonghoPark <pjhpjh0805@naver.com>
2023-08-17 17:15:55 +0900

    feat: 채팅 하단 입력창 컴포넌트에 메세지 publish 기능 추가 #144

--- src/components/Chatting/ChatRoomBottom/index.tsx
+++ src/components/Chatting/ChatRoomBottom/index.tsx
@@ -1,16 +1,41 @@
 import * as S from './style';
 import ChatSendButton from '@assets/icon/chatting-send.svg';
+import { publishMessage } from '@apis/chatting';
+import { useState } from 'react';
+import { PublishMessageType } from '@typing/chatting';
 
-
-const ChatRoomBottom = () => {
-    return (
-        <S.BottomWrapper>
-            <S.Input  placeholder="메세지를 입력하세요." />
-            <S.SendButton>
-                <ChatSendButton/>
-            </S.SendButton>
-        </S.BottomWrapper>
-    );
+interface Props {
+  roomId: string;
+  otherId: number;
+  myId: number;
 }
+
+const ChatRoomBottom = ({ roomId, otherId, myId }: Props) => {
+  const [messageContent, setMessageContent] = useState(''); // 입력된 메시지 상태 관리
+
+  const sendMessage = () => {
+    const message: PublishMessageType = {
+      roomId: roomId,
+      senderId: myId,
+      receiverId: otherId,
+      content: messageContent,
+    };
+    publishMessage(roomId, JSON.stringify(message));
+    setMessageContent('');
+  };
+
+  return (
+    <S.BottomWrapper>
+      <S.Input
+        value={messageContent}
+        onChange={(e) => setMessageContent(e.target.value)}
+        placeholder="메세지를 입력하세요."
+      />
+      <S.SendButton onClick={sendMessage}>
+        <ChatSendButton />
+      </S.SendButton>
+    </S.BottomWrapper>
+  );
+};
 
 export default ChatRoomBottom;
