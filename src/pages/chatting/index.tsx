0db6d426e3e13b0b56424fa71abe1569c2a6efc3
JeonghoPark <pjhpjh0805@naver.com>
2023-08-21 13:41:43 +0900

    feat: 채팅방 리스트 관련 컴포넌트 subscribe 로직 추가 #144

--- src/pages/chatting/index.tsx
+++ src/pages/chatting/index.tsx
@@ -3,29 +3,45 @@ import { Loader,Header, Navigation } from '@components/Common';
 import { ChatMain, ChatContent } from '@components/Chatting';
 import { useGetChattingList } from '@hooks/chatting';
 import { useState, useEffect } from 'react';
+import { subscribForList } from '@apis/chatting';
+import { useGetUserInfo } from '@hooks/common/queries';
+
 
 
 const Chatting = () => {
-  const { data: chattingListData, isError, refetch } = useGetChattingList();
+  const { data: chattingListData, isError, refetch, isFetching } = useGetChattingList();
   const [isPossibleToRendering, setIsPossibleToRendering] = useState(false);
+  const [chattingList, setChattingList] = useState([]);
+  const { currentUserId } = useGetUserInfo();
+
+
+  useEffect(() => {
+    console.log("현재 접속한 유저의 아이디 => ", currentUserId);
+    console.log("전역 채널 구독 완료");
+    subscribForList(currentUserId,() => {
+      console.log("New message received in global channel:");
+      setIsPossibleToRendering(false);
+    });
+  }, []);
 
   useEffect(() => {
     if (isError) {
       setIsPossibleToRendering(true);
     }
     if (chattingListData) {
+      setChattingList(chattingListData.data);
       setIsPossibleToRendering(true);
     } 
     if (!isPossibleToRendering) {
       refetch();
     }
-  }, [isError, chattingListData, isPossibleToRendering, refetch]);
+  }, [isError, chattingListData, isPossibleToRendering, refetch, isFetching]);
 
-  if (!isPossibleToRendering) {
+  if (!isPossibleToRendering || isFetching) {
     return <Loader />;
   }
 
-  const chattingList = chattingListData?.data;
+  console.log(chattingList);
 
   return (
     <ChatMain>
