0a216f648d091ba22fc24fde931285d42c4ec166
taeyoung <rlaxodud911@gmail.com>
2023-09-06 23:12:44 +0900

    [LAS-110] design: 로그인 페이지 기기별 레이아웃 추가 #160

--- src/components/Login/LoginNav/style.tsx
+++ src/components/Login/LoginNav/style.tsx
@@ -9,8 +9,6 @@ export const NavContainer = styled.div`
   align-items: center;
   justify-content: center;
   gap: 30px;
-
-  overflow: hidden;
 `;
 
 export const LogoContainer = styled.div`
@@ -39,13 +37,13 @@ const Button = styled.button`
   justify-content: center;
 
   width: 100%;
-  height: 62px;
+  padding: 10px;
 
   border: 0px;
   border-radius: 6px;
 
   font-weight: 400;
-  font-size: 20px;
+  font-size: 1rem;
   line-height: 30px;
   letter-spacing: -2%;
 
@@ -53,11 +51,13 @@ const Button = styled.button`
 
   svg {
     margin-right: 4px;
+    height: 1.5rem;
   }
 `;
 
 export const KakaoBtn = styled(Button)`
   background-color: var(--color-yellow);
+  color: var(--color-black);
 `;
 
 export const HomeBtn = styled(Button)`
