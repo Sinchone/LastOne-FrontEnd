ed5bf9b8fd4332b1b2709a71f36969209fcdb859
taeyoung <rlaxodud911@gmail.com>
2023-09-10 06:08:39 +0900

    [LAS-107] feat: 3대 숫자 범위 0~999로 설정 #162

--- src/hooks/MyPage/useProfileForm.ts
+++ src/hooks/MyPage/useProfileForm.ts
@@ -54,9 +54,13 @@ const useProfileForm = (initialState: MyPageField) => {
   };
 
   const handleChangeFitnessInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputEvent = e.nativeEvent as InputEvent;
+
+    if (inputEvent.data && !/\d/.test(inputEvent.data)) return;
+    if (+e.target.value > 999) return;
     setFitness({
       ...fitness,
-      [e.target.name]: e.target.value,
+      [e.target.name]: +e.target.value,
     });
   };
 
