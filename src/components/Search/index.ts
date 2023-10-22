8c802ff51fa094aa356ed5407d1a5badf9ae020b
taeyoung <rlaxodud911@gmail.com>
2023-10-22 22:16:21 +0900

    [LAS-12] fix: index.ts 내의 삭제한 컴포넌트 export 삭제 #181

--- src/components/Search/index.ts
+++ src/components/Search/index.ts
@@ -1,4 +1,3 @@
-export { default as Header } from './Header';
 export { default as Content } from './Content';
 export { default as Input } from './Input';
 export { default as History } from './History';
--- src/components/SearchGym/index.ts
+++ src/components/SearchGym/index.ts
@@ -1,3 +1,2 @@
-export { default as Header } from './Header';
 export { default as Input } from './Input';
 export { default as Layout } from './Layout';
